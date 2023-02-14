d6284baf294d32b3d0c374593d31b6fcec6f4997
dyzn3y <kdmarshall728@gmail.com>
2023-02-14 11:46:43 -0500

    made GET method for getting comments on posts

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -95,6 +95,19 @@ router.delete('/:id', async (req, res) => {
     }
 })
 
-
+router.get('/:id/comments', async (req, res) => {
+    try {
+        const post = await Posts.findById(req.params.id);
+        if (!post) {
+            res.status(404).json({ message: 'The post with the specified ID does not exist' })
+        } else {
+            const comments = await Posts.findPostComments(req.params.id)
+            res.json(comments)
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'The comments information could not be retrieved' })
+    }
+})
 
 module.exports = router
