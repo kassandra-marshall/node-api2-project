00b388cce4954023485fdf1a8bab104643458d3a
dyzn3y <kdmarshall728@gmail.com>
2023-02-14 10:01:19 -0500

    made GET method for posts by id

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -16,4 +16,19 @@ router.get('/', (req, res) => {
         });
 });
 
+router.get('/:id', (req, res) => {
+    Posts.findById(req.params.id)
+        .then(posts => {
+            if(posts) {
+                res.status(200).json(posts);
+            } else {
+                res.status(404).json({ message: 'The post with the specified ID does not exist'})
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({ message: 'The post information could not be retrieved'});
+        });
+});
+
 module.exports = router
